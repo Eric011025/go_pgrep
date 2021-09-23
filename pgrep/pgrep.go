b8e96f66d720d06aabd4c87be4b295a22a11a10d
Eric011025 <ericpark011025@gmail.com>
2021-09-23 22:08:59 +0900

    [FIX] change GetPidToPPid function using pgrep exec to vfs

--- pgrep/pgrep.go
+++ pgrep/pgrep.go
@@ -4,34 +4,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	"os/exec"
 	"strconv"
 	"strings"
 )
-
-// find pid using by ppid
-// this function using pgrep command
-func GetPidToPPid(ppid int) (pidList []os.Process, err error) {
-	pidCmdOut, err := exec.Command("sh", "-c", fmt.Sprintf("pgrep -P %d | awk '{ print $1 }' ", ppid)).Output()
-	if err != nil {
-		return
-	}
-	tmp := strings.Split(string(pidCmdOut), "\n")
-	for _, pidStr := range tmp[0 : len(tmp)-1] {
-		var pid *os.Process
-		var pidInt int
-		pidInt, err = strconv.Atoi(pidStr)
-		if err != nil {
-			return
-		}
-		pid, err = os.FindProcess(pidInt)
-		if err != nil {
-			return
-		}
-		pidList = append(pidList, *pid)
-	}
-	return
-}
 
 // get process list
 func GetPidList() (pList []os.Process, err error) {
@@ -51,7 +26,7 @@ func GetPidList() (pList []os.Process, err error) {
 }
 
 // find pid by ppid and kill all pid
-func KillPidToPPID(ppid int) (err error) {
+func KillPidToPPid(ppid int) (err error) {
 	pidList, err := GetPidToPPid(ppid)
 	if err != nil {
 		return
@@ -63,4 +38,19 @@ func KillPidToPPID(ppid int) (err error) {
 		}
 	}
 	return err
+}
+
+// find pid using by ppid
+func GetPidToPPid(ppid int) (pid []os.Process, err error) {
+	pList, err := GetPidList()
+	for pIndex, p := range pList {
+		statByte, err := ioutil.ReadFile(fmt.Sprintf("/proc/%d/stat", p.Pid))
+		if err != nil {
+			return nil, err
+		}
+		if strings.Split(string(statByte), " ")[3] == strconv.Itoa(ppid) {
+			pid = append(pid, pList[pIndex])
+		}
+	}
+	return
 }
