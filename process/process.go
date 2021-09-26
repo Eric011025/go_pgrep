8a451ecddb72321b6b61baad93930cc031e8d5cd
Eric011025 <ericpark011025@gmail.com>
2021-09-26 23:26:26 +0900

    [ADD] process kill function add

--- process/process.go
+++ process/process.go
@@ -1,23 +1,37 @@
 package process
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strconv"
 	"strings"
+	"sync"
+	"syscall"
 )
 
 type Process struct {
-	Pid  int
-	PPid int
-	Cmd  string
+	Pid   int
+	PPid  int
+	Cmd   string
+	sigMu sync.RWMutex
 }
 
-//
+var (
+	ProcessNotFound error = errors.New("process : process not founded")
+)
+
+const (
+	Kill = syscall.SIGKILL
+)
+
+// convert pid to process object
 func NewProcess(id int) (p Process, err error) {
+	// read process status
 	statByte, err := ioutil.ReadFile(fmt.Sprintf("/proc/%d/stat", id))
 	if err != nil {
-		return Process{}, err
+		return Process{}, ProcessNotFound
 	}
 
 	// pid init
@@ -36,4 +50,18 @@ func NewProcess(id int) (p Process, err error) {
 	return
 }
 
-// kill function
+// process Kill
+func (p Process) Kill() (err error) {
+	sig := os.Kill
+	signal, able := sig.(syscall.Signal)
+	if able == false {
+		return errors.New("process : unsupported signal type")
+	}
+	if err = syscall.Kill(p.Pid, signal); err != nil {
+		if err == syscall.ESRCH {
+			return errors.New("process : process is already dead")
+		}
+		return err
+	}
+	return
+}
